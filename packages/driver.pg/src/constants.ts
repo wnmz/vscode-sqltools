b2b97477756358677fda1d7b06c55221bae1c502
Daniil Parkov <damnwnm@gmail.com>
2024-10-16 12:50:26 +0700

    Rename driver

--- packages/driver.pg/src/constants.ts
+++ packages/driver.pg/src/constants.ts
@@ -1,7 +1,5 @@
 import { IDriverAlias } from '@sqltools/types';
 
 export const DRIVER_ALIASES: IDriverAlias[] = [
-  { displayName: 'PostgreSQL', value: 'PostgreSQL'},
-  { displayName: 'AWS Redshift', value: 'AWS Redshift'},
-  { displayName: 'CockroachDB', value: 'CockroachDB'},
+  { displayName: 'PostgreSQL (Rich Error)', value: 'PostgreSQL'},
 ];
